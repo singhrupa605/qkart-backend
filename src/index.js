a36ca4d9917d427f1f7a1e95c615f9eb4ac74c6d
singhrupa605 <singhrupa605@gmail.com>
2022-07-13 23:28:44 +0530

    ME_QKART_BACKEND_MODULE_TEST  (Crio.Do)

--- src/index.js
+++ src/index.js
@@ -7,24 +7,31 @@ let server;
 
 // TODO: CRIO_TASK_MODULE_UNDERSTANDING_BASICS - Create Mongo connection and get the express app to listen on config.port
 
-mongoose.connect(config.mongoose.url).then(
-  () => {
-    const logger = winston.createLogger({
-      level: "info",
-      transports: [new winston.transports.Console()],
-    });
+mongoose
+  .connect(config.mongoose.url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(
+    () => {
+      const logger = winston.createLogger({
+        level: "info",
+        transports: [new winston.transports.Console()],
+      });
 
-    logger.info("Connected to MongoDB");
+      logger.info("Connected to MongoDB");
 
-    app.listen(config.port, () => {
-      console.log("Server started at port  : " + config.port);
-    });
-  },
-  () => {
-     const logger = winston.createLogger({
-       level: "error",
-       transports: [new winston.transports.Console()],
-     });
-    logger.error("Cannot connect to mongoDB, please check your backend")
-  }
-);
+      app.listen(config.port, () => {
+        console.log("Server started at port  : " + config.port);
+      });
+    },
+    () => {
+      const logger = winston.createLogger({
+        level: "error",
+        transports: [new winston.transports.Console()],
+      });
+      logger.error("Cannot connect to mongoDB, please check your backend");
+    }
+  );
